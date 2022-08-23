14ce076cc6c36e5495a231e7ecd83220a1e112ba
shubham <sbmsangam786@gmail.com>
2022-08-23 10:22:43 +0545

    first commit

---
+++ src/main/java/com/learnspringboot/demoprojectone/DemoprojectoneApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.demoprojectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringboot/demoprojectone/DemoprojectoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.demoprojectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
