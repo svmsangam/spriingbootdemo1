5ae4dabaf649a98f2c828e6840e63925c4f5c827
shubham <sbmsangam786@gmail.com>
2022-08-23 10:41:21 +0545

    second commit

---
+++ src/main/java/com/learnspringboot/demoprojectone/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.learnspringboot.demoprojectone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
